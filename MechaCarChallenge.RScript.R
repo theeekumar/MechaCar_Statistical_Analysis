4ca976815825e8ab1d16c870f2346951c1e91cfe
Maya Kumar <mayakumar@Mayas-MacBook-Pro-4.local>
2022-04-02 23:40:28 -0700

    completed deliverable 2

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -2,3 +2,9 @@ library(dplyr)
 mc_mpg <- read.csv('MechaCar_mpg.csv',check.names = F,stringsAsFactors = F)
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mc_mpg) #generate mutilpe linear regression model
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mc_mpg)) #generate summary stats
+
+# Deliverable 2
+library(tidyverse)
+sus_coil <- read.csv('Suspension_Coil.csv') #read csv into table
+summarize_PSI <- sus_coil %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep') #create summary table
+sum_ManLot <- sus_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep') #create summary table
